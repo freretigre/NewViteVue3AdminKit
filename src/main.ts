74ea8ab189ec5d1cec0c80c4edb7f3f21ea5af1f
jecky <jecky@sing.com>
2022-02-20 14:09:27 +0700

    add files styles layout init css

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue'
 import { store } from './store'
 import routes from './routes'
-
 import App from './App.vue'
+import '@/styles/index.scss' // 全局 css
 
 const app = createApp(App);
 
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,24 @@
-import { useRouter, createRouter, createWebHashHistory } from 'vue-router';
+import { useRouter, createRouter,  createWebHashHistory, RouteRecordRaw } from 'vue-router';
+import Layout from '@/layout/index.vue'
 import Home from '../views/Home.vue';
 import About from '../views/About.vue';
-const routes = [
-    { path: '/', component: Home },
-    { path: '/about', component: About },
+const routes: Array<RouteRecordRaw>  = [
+    {
+        path: '/',
+        name: 'Layout',
+        component: Layout,
+        redirect: '/dashboard',
+        children: [
+            {
+                path: 'dashboard',
+                name: 'Dashboard',
+                component: () => import(/* webpackChunkName: "dashboard" */ '@/views/dashboard/index.vue'),
+                meta: {
+                    title: 'Dashboard'
+                }
+            }
+        ]
+    },
 ]
 
 const router = createRouter({
